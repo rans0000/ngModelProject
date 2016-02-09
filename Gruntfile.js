7011d80cbd1daaedfc9c95eb6721bf53ad8a59ec
rans0000 <rans0000@gmail.com>
2016-02-09 19:53:05 +0530

    SCSS added & HTML moved to root
    Changed * html folder moved to project root
    Added * grunt-sass and grunt-contrib-watch added in build

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,44 @@
+//require('load-grunt-tasks')(grunt);
+
 module.exports = function(grunt) {
 
-    grunt.initConfig({
-        express: {
-            server: {
-                options: {
-                    port: 9001,
-                    bases: 'dev',
-                    open: 'http://localhost:9001'
-                }
-            }
-        }
-    });
+	grunt.initConfig({
+		sass: {
+			options: {
+				sourceMap: true,
+				outputStyle: 'compressed',
+			},
+			dist: {
+				files: {
+					'html/css/style.css': 'html/scss/style.scss'
+				}
+			}
+		},
+		watch: {
+			html: {
+				files: ['html/scss/**/*.scss'],
+				tasks: ['sass'],
+				options: {
+					spawn: false,
+				},
+			},
+		},
+		express: {
+			server: {
+				options: {
+					port: 9001,
+					bases: 'dev',
+					open: 'http://localhost:9001'
+				}
+			}
+		}
+	});
 
-    grunt.loadNpmTasks('grunt-express');
+	grunt.loadNpmTasks('grunt-express');
+	grunt.loadNpmTasks('grunt-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 
-    grunt.registerTask('default', ['express', 'express-keepalive']);
-    grunt.registerTask('myserver', ['express', 'express-keepalive']);
+	grunt.registerTask('default', ['sass', 'express', 'express-keepalive']);
+	grunt.registerTask('myserver', ['express', 'express-keepalive']);
+	grunt.registerTask('devhtml', ['watch:html', 'express', 'express-keepalive']);
 };
